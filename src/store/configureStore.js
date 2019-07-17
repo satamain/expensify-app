242fcc6080080c093df415320dde4c4e969ef8c5
Andrei Miranda <drei@sataMain.local>
2019-07-17 08:43:12 -0300

    a lot of stuff

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,20 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import expensesReducer from '../reducers/expenses'
-import filtersReducer from '../reducers/filters'
-import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+import authReducer from '../reducers/auth';
 
-const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
-      filters: filtersReducer
+      filters: filtersReducer,
+      auth: authReducer
     }),
-    composeEnhacers(applyMiddleware(thunk))
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-  return store
-}
+    composeEnhancers(applyMiddleware(thunk))
+  );
+
+  return store;
+};
